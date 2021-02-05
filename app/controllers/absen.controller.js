df7cdf484fb39c44b133c981b19ddb4a89101468
rizkirizaluddin <rizki.rizal17@gmail.com>
2021-02-05 09:02:03 +0700

    first commit

--- app/controllers/absen.controller.js
+++ app/controllers/absen.controller.js
@@ -29,7 +29,9 @@ exports.create = (req, res) => {
   
     // Create a Tutorial
     const absen = {
-      username: req.body.username
+      username: req.body.username,
+      lat: req.body.lat,
+      lng: req.body.lng
     };
   
     // Save Tutorial in the database
